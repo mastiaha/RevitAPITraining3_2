d274b39fa0b4dd58863e7c05969428f91f070e63
Yovanovich.aa <Yovanovich.aa@DESKTOP-ITANAVJ>
2022-03-29 18:31:37 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining32/Main.cs
@@ -0,0 +1,43 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining32
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedRef = uidoc.Selection.PickObject(ObjectType.Element, "Веберите трубу");
+            var selectedElement = doc.GetElement(selectedRef);
+
+            if (selectedElement is Pipe)
+            {
+                Parameter lengthParameter = selectedElement.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH);
+                if (lengthParameter.StorageType == StorageType.Double)
+                {
+                    double lengthValue = UnitUtils.ConvertFromInternalUnits(lengthParameter.AsDouble(), UnitTypeId.Meters);
+                    TaskDialog.Show("Длина труб", lengthValue.ToString());
+                    //TaskDialog.Show("Объем2", lengthParameter2.AsDouble().ToString());
+                }
+                
+            }
+            
+
+                return Result.Succeeded;
+        }
+    }
+
+}
